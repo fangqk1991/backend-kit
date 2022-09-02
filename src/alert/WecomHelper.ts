fc0f627eff1875ba1bbce9a06d046c599bcbbe89
Fang <me@fangqk.com>
2022-09-02 20:49:44 +0800

    (main): v0.28.0 - WecomHelper.makeWecomProxy + env

--- src/alert/WecomHelper.ts
+++ src/alert/WecomHelper.ts
@@ -2,12 +2,13 @@ import { WecomProxy } from './WecomProxy'
 import { _FangchaState } from '../main'
 
 export class WecomHelper {
-  public static makeWecomProxy(botKey: string) {
+  public static makeWecomProxy(botKey: string, env?: string) {
+    env = env || _FangchaState.env
     const proxy = new WecomProxy({})
-    if (!['staging', 'production'].includes(_FangchaState.env)) {
+    if (!['staging', 'production'].includes(env)) {
       proxy.setMuteMode(true)
     }
-    proxy.setTag(_FangchaState.env)
+    proxy.setTag(env)
     proxy.setRetainedBotKey(botKey)
     return proxy
   }
