041f38ddea9d88b7ab29a59b4d6dc5e1dbd33461
Fang <me@fangqk.com>
2022-09-04 16:21:54 +0800

    (profile): v0.30.0 +KitProfileSpecs

---
+++ src/apis/KitProfileApis.ts
@@ -0,0 +1,7 @@
+export const KitProfileApis = {
+  BasicProfileGet: {
+    method: 'GET',
+    route: '/api/v1/profile/basic-info',
+    description: '获取基础信息',
+  },
+}
--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,1 +1,2 @@
 export * from './KitSsoApis'
+export * from './KitProfileApis'
