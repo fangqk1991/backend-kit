398fab1a553075edeb4175a1a03cdf7008da0985
Fang <me@fangqk.com>
2022-11-09 00:57:45 +0800

    (package.json): add dependencies

---
+++ config/default.js
@@ -0,0 +1,10 @@
+module.exports = {
+  baseURL: 'http://localhost:16130',
+  backendPort: 16130,
+  jwtKey: 'backend_kit_jwt',
+  jwtSecret: 'some-jwt-secret',
+  AuthSDK: {
+    usernameRetained: 'admin',
+    passwordRetained: 'admin',
+  },
+}
