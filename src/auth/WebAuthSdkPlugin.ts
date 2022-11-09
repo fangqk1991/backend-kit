35a8165f95a7c3cfcdf062f52092d8d8592ef162
Fang <me@fangqk.com>
2022-11-09 11:43:47 +0800

    v0.33.2: use @fangcha/router@0.18.1

--- src/auth/WebAuthSdkPlugin.ts
+++ src/auth/WebAuthSdkPlugin.ts
@@ -2,12 +2,13 @@ import { AppPluginProtocol } from '../basic'
 import { WebAuthProtocol } from './WebAuthProtocol'
 import { _WebAuthState } from './_WebAuthState'
 import { WebAuthSpecDocItem } from './WebAuthSpecs'
+import { JwtSessionSpecDocItem } from '@fangcha/router/lib/main/JwtSessionSpecs'
 
 export const WebAuthSdkPlugin = (options: WebAuthProtocol): AppPluginProtocol => {
   return {
     appDidLoad: async () => {
       _WebAuthState.setAuthProtocol(options)
     },
-    specDocItems: [WebAuthSpecDocItem],
+    specDocItems: [WebAuthSpecDocItem, JwtSessionSpecDocItem],
   }
 }
