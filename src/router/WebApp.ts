ee3bac6b60e991c1763f364cb135e19746dab10d
Fang <me@fangqk.com>
2022-11-16 15:28:23 +0800

    v0.43.0: WebApp options + useJwtSpecs

--- src/router/WebApp.ts
+++ src/router/WebApp.ts
@@ -5,16 +5,20 @@ import { KitProfileSpecDocItem } from '../profile'
 import { _RouterState } from './_RouterState'
 import { RouterSdkOptions } from './RouterSdkOptions'
 import { RouterPlugin } from './RouterPlugin'
+import { JwtSessionSpecDocItem } from './retained-specs/JwtSessionSpecs'
 
 export class WebApp extends FangchaApp {
   routerPlugin: RouterPlugin
 
-  public constructor(protocol: AppProtocol & { routerOptions: RouterSdkOptions }) {
+  public constructor(protocol: AppProtocol & { useJwtSpecs?: boolean; routerOptions: RouterSdkOptions }) {
     super(protocol)
 
     const routerApp = _RouterState.routerApp
     routerApp.addDocItem(HealthDocItem)
     routerApp.addDocItem(KitProfileSpecDocItem)
+    if (protocol.useJwtSpecs) {
+      routerApp.addDocItem(JwtSessionSpecDocItem)
+    }
 
     this.routerPlugin = new RouterPlugin({
       ...protocol.routerOptions,
