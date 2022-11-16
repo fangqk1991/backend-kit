9c76024de343d58a88e58231d14b40607a65f0f1
Fang <me@fangqk.com>
2022-11-16 16:42:42 +0800

    (web-auth): demo + frontendConfig

--- demo/apps/web-auth-demo.ts
+++ demo/apps/web-auth-demo.ts
@@ -4,10 +4,18 @@ import { WebApp } from '../../src/router'
 import { AccountServer } from '@fangcha/account'
 import { MyDatabase } from '../services/MyDatabase'
 import { AuthMode } from '@fangcha/account/lib/common/models'
+import { _FangchaState } from '../../src'
 
 const app = new WebApp({
   env: 'development',
   appName: 'web-auth',
+  appDidLoad: async () => {
+    _FangchaState.frontendConfig = {
+      appName: 'Auth Demo',
+      background: 'linear-gradient(to top, #fdcbf1 0%, #fdcbf1 1%, #e6dee9 100%)',
+      logoCss: 'radial-gradient(circle 248px at center, #16d9e3 0%, #30c7ec 47%, #46aef7 100%)',
+    }
+  },
   routerOptions: {
     baseURL: DemoConfig.baseURL,
     backendPort: DemoConfig.backendPort,
